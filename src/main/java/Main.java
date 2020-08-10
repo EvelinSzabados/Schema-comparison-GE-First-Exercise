1eec21fe2afb681f1e438f4426003de070a2c32d
Evelin Szabados <szabadosevelin7@gmail.com>
2020-08-10 14:23:21 +0200

    Implement user interface for terminal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,42 @@
-public class Main {
-    public static void main(String[] args) {
+import view.UserInterface;
 
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        UserInterface ui = new UserInterface(System.in, System.out);
+        new Main(ui).run();
+    }
+    UserInterface ui;
+
+
+    Main(UserInterface ui) throws SQLException {
+        this.ui = ui;
+
+    }
+    private void run() throws SQLException {
+
+        boolean running = true;
+
+        while (running) {
+            ui.printTitle("Menu");
+            ui.printOption('a', "Table comparison");
+            ui.printOption('b', "Column comparison");
+            ui.printOption('q', "Exit");
+            switch (ui.choice("abq")) {
+                case 'a':
+                    ui.printTitle("Shows if new tables were added, or how many tables were deleted.");
+                    ui.println("0 tables were added or deleted");
+                    break;
+                case 'b':
+                    ui.printTitle("Shows if columns were modified.");
+                    ui.println("0 changes in columns");
+                    break;
+                case 'q':
+                    running = false;
+                    ui.println("See you next time!");
+                    break;
+            }
+        }
     }
 }
--- src/main/java/view/UserInterface.java
+++ src/main/java/view/UserInterface.java
@@ -1,4 +1,43 @@
 package view;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class UserInterface {
+    Scanner scanner;
+    PrintStream out;
+    String RESET = "\u001B[0m";
+    String GREEN = "\u001B[92m";
+    String BLUE = "\u001B[94m";
+
+    public UserInterface(InputStream in, PrintStream out) {
+        this.scanner = new Scanner(in);
+        this.out = out;
+    }
+
+    public void println(Object obj) {
+        out.println(GREEN + obj + RESET);
+    }
+
+    public void printTitle(String title) {
+        out.println(BLUE + "\n -- " + title + " --" + RESET);
+    }
+
+    public void printOption(char option, String description) {
+        out.println("(" + option + ")" + " " + description);
+    }
+
+    public char choice(String options) {
+        String line;
+        do {
+            out.print(BLUE + "Options [" + options + "]: " + RESET);
+            line = scanner.nextLine();
+        } while (!(line.length() == 1 && options.contains(line)));
+        return line.charAt(0);
+    }
+
+    private void printPrompt(String prompt, Object defaultValue) {
+        System.out.print(prompt + " [" + defaultValue + "]: ");
+    }
 }
