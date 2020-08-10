560020bb6996f7b738f4a12611ab6b91dda2ea95
Evelin Szabados <szabadosevelin7@gmail.com>
2020-08-10 16:40:09 +0200

    Implement method to set tables for database

--- src/main/java/controller/DataManager.java
+++ src/main/java/controller/DataManager.java
@@ -1,13 +1,34 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import model.Database;
+import model.Table;
+
+import java.sql.*;
 
 public class DataManager {
-    public Connection conn;
+    private Connection conn = connection();
 
-    public DataManager(Connection connect) {
-        this.conn = connect;
+    public DataManager() throws SQLException {
+    }
+
+    public void setTablesForDatabase(Database database) throws SQLException {
+        String query = "SELECT table_name FROM information_schema.tables WHERE table_schema=?;";
+
+        PreparedStatement st = conn.prepareStatement(query);
+        st.setString(1, database.getName());
+
+        ResultSet rs = st.executeQuery();
+
+        while (rs.next()) {
+            database.addTables(new Table(rs.getString("table_name"),database.getName()));
+        }
+        rs.close();
+    }
+
+    public void setColumnsForTable(Table table){
+
+    }
+    private Connection connection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mariadb://localhost/information_schema", "evelin", "password");
     }
 }
