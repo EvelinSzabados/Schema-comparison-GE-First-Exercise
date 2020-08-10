faf71f5c44b19c0da725b281d67a04556088e11e
Evelin Szabados <szabadosevelin7@gmail.com>
2020-08-10 16:40:38 +0200

    Create pojo for column

--- src/main/java/model/Column.java
+++ src/main/java/model/Column.java
@@ -1,4 +1,35 @@
 package model;
 
+import controller.DataManager;
+
 public class Column {
+
+    private String name;
+
+    private String dataType;
+    private int maxCharLength;
+    private boolean isNullable;
+
+    public Column(String name, String dataType, int maxCharLength, boolean isNullable, DataManager dataManager){
+        this.name = name;
+        this.dataType = dataType;
+        this.maxCharLength = maxCharLength;
+        this.isNullable = isNullable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDataType() {
+        return dataType;
+    }
+
+    public int getMaxCharLength() {
+        return maxCharLength;
+    }
+
+    public boolean isNullable() {
+        return isNullable;
+    }
 }
