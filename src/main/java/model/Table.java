f98ceeb6941ad8a09b6c2bf6c24c696a89562140
Evelin Szabados <szabadosevelin7@gmail.com>
2020-08-10 16:41:41 +0200

    Create pojo for tables

--- src/main/java/model/Table.java
+++ src/main/java/model/Table.java
@@ -1,4 +1,26 @@
 package model;
 
+import java.util.List;
+
 public class Table {
+
+    private String name;
+    private String databaseName;
+    private List<Column> columns;
+
+    public Table(String name,String databaseName){
+        this.name = name;
+        this.databaseName = databaseName;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public List<Column> getColumns(){
+        return this.columns;
+    }
+    public void addColumns(Column newCol ){
+        this.columns.add(newCol);
+    }
 }
