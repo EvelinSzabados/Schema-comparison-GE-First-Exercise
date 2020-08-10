f9d1bc7f45939e4485754c856d7818180c07d017
Evelin Szabados <szabadosevelin7@gmail.com>
2020-08-10 16:41:16 +0200

    Set tables for db in constructor

--- src/main/java/model/Database.java
+++ src/main/java/model/Database.java
@@ -1,4 +1,32 @@
 package model;
 
+import controller.DataManager;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
+    private String name;
+    private List<Table> tables = new ArrayList<Table>();
+
+
+    public Database(String name, DataManager dataManager) throws SQLException {
+        this.name = name;
+        dataManager.setTablesForDatabase(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Table> getTables() {
+        return tables;
+    }
+
+    public void addTables(Table newTable) {
+        this.tables.add(newTable);
+    }
+
+
 }
